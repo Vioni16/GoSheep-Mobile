1120ab2fbcc352202f32047c0967be5ac594e0ff
gabriell74 <gabrielvivaldi07@gmail.com>
2026-04-18 01:57:31 +0700

    Menambahkan class exception, model API response, route untuk halaman cage, dan widget AsyncStateSliver

---
+++ lib/core/widgets/async_state_sliver.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AsyncStateSliver<T> extends StatelessWidget {
+  final bool isLoading;
+  final String? error;
+  final List<T> data;
+
+  final Widget Function() onLoading;
+  final Widget Function(String err) onError;
+  final Widget Function() onEmpty;
+  final Widget Function(List<T> data) onSuccess;
+
+  const AsyncStateSliver({
+    super.key,
+    required this.isLoading,
+    required this.error,
+    required this.data,
+    required this.onLoading,
+    required this.onError,
+    required this.onEmpty,
+    required this.onSuccess,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading && data.isEmpty) {
+      return onLoading();
+    }
+
+    if (error != null && data.isEmpty) {
+      return onError(error!);
+    }
+
+    if (data.isEmpty) {
+      return onEmpty();
+    }
+
+    return onSuccess(data);
+  }
+}
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:gosheep_mobile/features/authentication/screens/login_screen.dart';
+import 'package:gosheep_mobile/features/cage/screens/cage_screen.dart';
 import 'package:gosheep_mobile/features/main_navigation.dart';
 
 class AppRoutes {
   static const String home = '/home';
   static const String login = '/login';
+  static const String cage = '/cage';
 
   static Map<String, WidgetBuilder> routes = {
     login: (context) => LoginScreen(),
     home: (context) => MainNavigation(),
+    cage: (context) => CageScreen(),
   };
 }
